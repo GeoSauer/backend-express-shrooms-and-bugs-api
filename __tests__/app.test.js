60d0e8fa996d08cd334e8977407bf08c74bb9da8
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-03 12:00:07 -0600

    add mushrooms-data.js and get all mushrooms test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,21 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { mushrooms } = require('../lib/mushrooms-data.js');
+
+describe('mushroom routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/mushrooms should return a list of mushrooms', async () => {
+    const res = await request(app).get('/mushrooms');
+    const expected = mushrooms.map((mushroom) => {
+      return { id: mushroom.id, name: mushroom.common_name };
+    });
+    expect(res.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
